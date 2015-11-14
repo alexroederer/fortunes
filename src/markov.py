d0a2da5594a5dcbac84d57026fa9be999a1e46c6
alex <alex.roederer@gmail.com>
2015-11-14 01:42:47 -0500

    Markov Chain generator complete

--- src/markov.py
+++ src/markov.py
@@ -1,29 +1,78 @@
 '''
-Created December 10, 2013
+Created November 13, 2015 
 
-A file for running a learning Markov Chains. Practice. 
+Creates a Markov Chain from Text
 
 @author: Alex Roederer
 '''
 
-import numpy as np
+#import numpy as np
+import re
+import collections
+import random
 
 class MarkovChain:
-    sensorModel = np.array([])
-    sensorModel = np.array()
-    
-    def __init__(self, sensorModel, transitionModel):
-        pass
-    
-    def learnHMM(self, data):
-        pass
-    
-    def predictStates(self, data):
-        pass
 
+    occuranceProbs = collections.defaultdict(lambda: [])
+    
+    def __init__(self, tupleSize):
+        self.tupleSize = tupleSize
+    
+    def learn(self, dataFile):
+        #Opens provided data file
+        pattern = re.compile(r"[\w']+|[.,!?;]")
+        tokens = []
+        #Get all the words/punctuations from file
+        with open(dataFile, 'r') as dataHandle:
+            for line in dataHandle: 
+                tokens.extend(line.split())
+                #tokens.extend(pattern.findall(line))
+        #Break up into tuples; add next word to tuple's 'next words' list
+        for i in range(0, len(tokens)-self.tupleSize-1):
+            wordSet = tuple(tokens[i:i+self.tupleSize])
+            self.occuranceProbs[wordSet].append(tokens[i+self.tupleSize]) 
+
+    def predictNext(self, prevTuple):
+        possibleNexts = self.occuranceProbs[prevTuple]
+        #If there are none, return None
+        if len(possibleNexts) is 0:
+            return None
+        else:
+            #Select one at random
+            nextWord = possibleNexts[random.randint(0, len(possibleNexts)-1)]
+            return nextWord
+
+    def returnRandomStart(self):
+        #Returns a random 'starting' tuple from all seen tuples. 
+        starts = self.occuranceProbs.keys()
+        selected = random.randint(0, len(starts))
+        return starts[selected]
+
+    def generateSequence(self, start, seqLength):
+        outSequence = []
+        curVal = start
+        for i in range(0, seqLength):
+            result = self.predictNext(curVal)
+            outSequence.append(result)
+            newVal = list(curVal[1:])
+            newVal.append(result)
+            curVal = tuple(newVal)
+        return outSequence
+
+    def __str__(self):
+        outputString = '------------Markov Chain Object-------------\n'
+        #Add a nice printout representation of the object
+        #Possibly with most likely tuples? 
+        outputString = outputString + '' 
+        return outputString
 
 if __name__ == "__main__":
-    print "Begin test sequence"
-    pass
+    print "testChain variable initiated with test MarkovChain object"
     
-    potato = MarkovChain(3)
+    testChain = MarkovChain(1)
+    testChain.learn('../data/fortunes.txt')
+    testChain.learn('../data/fortunes2.txt')
+    testChain.learn('../data/fortunes.txt')
+    testChain.learn('../data/fortunes2.txt')
+
+    #testChain.learn('../data/analecs.txt')
--- src/twitter_bot.py
+++ src/twitter_bot.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
         getSecrets(secretLocations)
 
     #Authenticate twitter account
+    #Note: Only do this 
     #auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     #auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     #api = tweepy.API(auth)
