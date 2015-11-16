37938dc8e5726ccd05ba3a8f5e29355ab7172c01
alex <alex.roederer@gmail.com>
2015-11-16 00:52:02 -0500

    Fixing bug where bot would repeatedly tweet at people; caused by notifications being sold from new to old rather than old to new. Logging also added to help debug.

--- src/twitter_bot.py
+++ src/twitter_bot.py
@@ -2,6 +2,8 @@
 
 import tweepy
 import time
+import logging
+import datetime
 from markov import MarkovChain 
 from secrets import secrets
 
@@ -36,35 +38,48 @@ def generateFortune(markovChain, seedWord=None):
 
 if __name__ == '__main__':
 
+    #Set up logging
+    logging.basicConfig(filename='logs/' + str(datetime.datetime.now()) + '.log', level=logging.DEBUG)
+
     #Create and train markov chain
     fortuneChain = MarkovChain(1)
     fortuneChain.learn('../data/fortunes.txt')
+    logging.info('Markov Chain Trained')
 
     #Authenticate twitter account
     #Note: Only do this when ready to go live!
     auth = tweepy.OAuthHandler(secrets['CONSUMER_KEY'], secrets['CONSUMER_SECRET'])
     auth.set_access_token(secrets['ACCESS_TOKEN'], secrets['ACCESS_TOKEN_SECRET'])
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    logging.info('Authentication Successful')
 
     mostRecentReply = 0
-    waitTime = 5 * 60
+    waitTime = 2 * 60
+    logging.info('Wait time set to ' + str(waitTime) + ' seconds')
 
     #Run forever
     while True:
     #if True: 
-
-        #Bot flow: 
-        #Get tweets directed at account since last check
+        #If we're on startup, fastforward to current tweet 
         if mostRecentReply is 0:
+            #Grab all mentions
             mentions = api.mentions_timeline()
-        else:
-            mentions = api.mentions_timeline(since_id = mostRecentReply)
+            #Do not reply to these tweets (as they are old)
+            if len(mentions) > 0:
+                mostRecentReply = mentions[0].id
+                logging.info('Fast-forwarding most recent reply to ' + str(mostRecentReply))
+
+        #Get tweets directed at account since last check
+        mentions = api.mentions_timeline(since_id = mostRecentReply)
+        mentions.reverse()
         for mention in mentions:
-            print mention.text
-            print mention.author.screen_name
+            #print mention.text
+            #print mention.author.screen_name
+            logging.info(str(mention))
             
             #Generate a fortune 
             fortune = generateFortune(fortuneChain)
+            logging.info(str(fortune))
 
             #Send that user a reply with their fortune
             statusRet = api.update_status(status='@' + mention.author.screen_name + 
@@ -73,7 +88,7 @@ if __name__ == '__main__':
         #Update most recent reply if there's something newer
         if len(mentions) > 0: 
             mostRecentReply = mentions[-1].id
+            logging.info('Updating most recent reply to ' + str(mostRecentReply))
         
         #Wait for a period before looping again
         time.sleep(waitTime)
-#END SCRIPT
